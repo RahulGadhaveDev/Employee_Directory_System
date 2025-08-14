0cacf531fe60caa9e9e96f1235468bdac54925ee
Rahul Gadhave <rahulgadhave022@gmail.com>
2025-08-14 22:41:11 +0530

    some

--- src/main/java/com/Repository/EmployeeRepo.java
+++ src/main/java/com/Repository/EmployeeRepo.java
@@ -60,7 +60,7 @@ public class EmployeeRepo {
 		transaction.commit();
 		session.close();
 	}
-	
+	//get Employee data 
 	public void getEmployee() {
 
 		SessionFactory factory = EmployeeUtility.getfactory();
